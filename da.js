3a77890799c1070680bb0f0983d58e13db30e8c3
yamazaki_daisuke <yamazaki.daisuke@bigb.jp>
2017-06-28 16:07:04 +0900

    no message

--- da.js
+++ da.js
@@ -12,7 +12,7 @@ var p; // shortcut to reference prototypes
 		
 		
 		
-		function fl_MouseClickHandler{
+		function fl_MouseClickHandler(event:MouseEvent):void; {
 			sikaku.gotoAndPlay("go");
 		}
 	}
